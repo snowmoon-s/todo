9da27482780219009c2ed189600256b9b1280123
snowdog <snowdog@biccotakko.link>
2020-07-09 20:03:31 +0900

    day1 fin

--- src/js/components/Cards.tsx
+++ src/js/components/Cards.tsx
@@ -1,7 +1,8 @@
-import { Card, CardContent } from '@material-ui/core'
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core'
 import { Task } from '../ models/Task'
+import { Button, Card, CardContent, TextField } from '@material-ui/core'
+import { Edit, Done } from '@material-ui/icons'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -9,11 +10,14 @@ const useStyles = makeStyles(theme => ({
   },
   card: {
     margin: theme.spacing(1),
-    border: '1px solid #333',
-    borderRadius: '20px',
+    padding: theme.spacing(1),
   },
   cardTitle: {
     padding: theme.spacing(1),
+  },
+  cardContainer: {
+    display: 'flex',
+    justifyContent: 'center',
   },
 }))
 
@@ -23,11 +27,27 @@ interface Props {
 
 export const Cards = ({ task }: Props) => {
   const classes = useStyles()
-  // const [isEditing, setIsEditing] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+  const [inputTask, setInputTask] = useState(task.name)
 
   return (
     <Card className={classes.card} raised>
-      <CardContent className={classes.cardTitle}>{task.name}</CardContent>
+      {isEditing ? (
+        <div className={classes.cardContainer}>
+          <TextField
+            onChange={e => setInputTask(e.target.value)}
+            value={inputTask}
+            label="edit table name"
+            margin="normal"
+          />
+          <Button onClick={() => setIsEditing(false)}>{<Done />}</Button>
+        </div>
+      ) : (
+        <div className={classes.cardContainer}>
+          <CardContent className={classes.cardTitle}>{task.name}</CardContent>
+          <Button onClick={() => setIsEditing(true)}>{<Edit />}</Button>
+        </div>
+      )}
     </Card>
   )
 }
